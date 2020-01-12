dfc26708d1ed279798f64ce6f992a48cb0b7150a
Mark Keeble <markkeeble14@gmail.com>
2020-01-11 20:38:49 -0800

    Commit with both main and map activity.

--- app/src/main/java/com/example/besafe/MainActivity.java
+++ app/src/main/java/com/example/besafe/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.besafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -18,5 +21,10 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("BeSafe");
         myRef.setValue("Hello, World!");
 
+    }
+
+    public void onClick(View view) {
+        Intent i = new Intent(this, MapsActivity.class);
+        startActivity(i);
     }
 }
